acf2ee1c5959228a4691ae5fa916c01df7906620
AHafsaoui <88088032+AHafsaoui@users.noreply.github.com>
2021-10-29 11:30:10 +0200

    Update sparql_unicorn_dialog.py

--- sparql_unicorn_dialog.py
+++ sparql_unicorn_dialog.py
@@ -48,7 +48,7 @@ from .loadgraphdialog import LoadGraphDialog
 
 # This loads your .ui file so that PyQt can populate your plugin with the elements from Qt Designer
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
-    os.path.dirname(__file__), 'sparql_unicorn_dialog_base_simp.ui'))
+    os.path.dirname(__file__), 'sparql_unicorn_dialog_base.ui'))
 
 ##
 #  @brief The main dialog window of the SPARQLUnicorn QGIS Plugin.
